591c31dca3f31b611b0d188cdc29f2eb17a64dd6
allandereal <ahullan@gmail.com>
2023-04-08 03:45:20 +0300

    add logic to fetch outstanding loans

---
+++ app/Http/Controllers/LoanController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\GetLoansRequest;
+use App\Http\Resources\LoanResource;
+use App\Models\Account;
+use App\Models\ApiRequest;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+
+class LoanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(GetLoansRequest $request)
+    {
+        $account = Account::whereHas('outstandingLoans')->with('outstandingLoans')
+            ->firstWhere('number', '=', $request->validated('account_number'));
+
+        if ($account){
+            ApiRequest::addNew('positive');
+            return LoanResource::collection($account->outstandingLoans);
+        }
+
+        ApiRequest::addNew('negative');
+
+        return response()->json('no loan found', Response::HTTP_OK);
+    }
+}
